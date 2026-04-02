18a4f12e7b78ca929954b05eec18c3831804b4b9
Ricardo Avila <ricardoavilabazalar@gmail.com>
2026-04-02 16:49:01 +0200

    feat: creacion de archivo 13 - modulos

--- Actividades/12-excepciones.py
+++ Actividades/12-excepciones.py
@@ -1,24 +1,29 @@
 print("\n******* Excepciones.py *******\n")
 
-print("""
+print(
+    """
 - En programación, es normal encontrar los errores. 
 - Los lenguajes de programación incluyen formas de detectar, controlar y gestiona errores
 - Evitando que el programa se detenga de forma inesperada
-""")
+"""
+)
 
-print("""
+print(
+    """
 **** Estructura general del manejo de excepciones en Python ****
     - try -> Intenta ejecutar el código // "Lo intento"
     - except -> Ejecuta si hay error // "Si falla, lo controlo"
     - else -> Ejecuta si todo va bien // "Si todo va bien, sigo"
     - finally -> Ejecuta siempre  // "Esto se ejecuta si o si"
-""")
+"""
+)
 print("======== Cambio de tipo de dato en una variable =========")
 numberOne = 5
 numberTwo = 1
 numberTwo = "1"
 
-print("""
+print(
+    """
 # Aqui dara un error al querer sumar int + string
 
     print(numberOne + numberTwo)
@@ -26,10 +31,11 @@ print("""
 dando un: TypeError: unsupported operand type(s)for +: 'int' and 'str'
 * En un programa pequeño se detendria esa parte del codigo pero en un programa 
 grande provocaría funcionalidades completas.
-""")
+"""
+)
 
 print("======== Controlando errores con try / except =========")
-# Para evitar que el porograma; se cierre cuando ocurre un error podemos usar 
+# Para evitar que el porograma; se cierre cuando ocurre un error podemos usar
 # el manejo de excepciones
 
 print("# try exept#")
@@ -39,7 +45,7 @@ try:
 
 # se ejecuta si se produce una exepción
 except:
-        print("Se ha producido un error")
+    print("Se ha producido un error")
 
 
 print("\n====== #try exept else# =======")
@@ -48,13 +54,13 @@ try:
     print("No se ha producido un error")
 
 except:
-# se ejecuta si se produce una exepción
+    # se ejecuta si se produce una exepción
     print("Se ha producido un error")
 else:
-# se ejecuta si no se ejecuta una exepción
+    # se ejecuta si no se ejecuta una exepción
     print("La ejecución continúa correctamente")
 finally:
-# se ejecuta siempre
+    # se ejecuta siempre
     print("Fin del bloque de control de excepciones")
 
 
@@ -64,21 +70,23 @@ try:
     print(numberOne + numberTwo)
     print("No se ha producido un error")
 except:
-# se ejecuta si se produce una exepción
+    # se ejecuta si se produce una exepción
     print("Se ha producido un error")
 else:
-# se ejecuta si no se ejecuta una exepción
+    # se ejecuta si no se ejecuta una exepción
     print("La ejecución continúa correctamente")
 finally:
-# se ejecuta siempre
+    # se ejecuta siempre
     print("Fin del bloque de control de excepciones")
 
-print("""
+print(
+    """
 ""\n======= # Excepciones por tipo =======
     - Permite al programa ser mas preciso el control de errores
     - La captura del tipo de error se guarda en una variable
     - Al ocurrir un ValueError se guarda en la variable error
-""")
+"""
+)
 numberOne = 1
 numberTwo = 2
 try:
--- Actividades/13-Modulo.py
+++ Actividades/13-Modulo.py
@@ -1,48 +1,107 @@
-# """
-# Que es un modulo?
-# """
+print("\n********** Modulos ************\n")
 
-# def my_function():
+print("""
+¿Qué es un módulo?
+------------------
+- Forma de organizar código propio o externo
+- Al crecer un programa se tiene que hacer claro, mantenible y escalable
+- Siendo la idea principal poder importar los archivos en otros programas
+- Se evita repetir código y facilitar el desarrollo de aplicaciones a mas grande
 
-# # recordar
+* Si se quiere reutilizar código de un archivo en otro, con 'def' se define la función
+""")
+def my_function():
+    print("Esto es una función")
 
-# def sum_two_values(first_number)
-
-
-# def sum_two_values(first_number, second_number):
-#     print(first_number + second_number)
-
-# sum_two_values(5, 7)
-# sum_two_values(54754, 71231)
-# sum_two_values(1.4, 5.2)  # tipo float
-
-# #solucion
-
-# """
-# este
-# """
-
-# #El problema
+my_function()
+my_function()
+my_function()    
 
 
-# import
+print("""
+** Al copiar el código y llevarse a un nuevo archivo al intentar llamar a la 
+    función directamente sin importarla, el programa dará 'error' al no reconocerla
+""")
+# modules#de  my_function():
+my_function() #x Error: name 'my_function' is not defined
 
-import Actividades.my_module as my_module
+print("""
+*** Recordar: que en my_function se tenia una función sencilla 
+    sumar dos valores recibidos como parámetros
+""")
+def sum_two_values(first_number, second_number):
+    print(first_number + second_number)
+print("""
+- Si se necesitare en otro archivo, la solución instintiva es copiar y pegar
+- Produciendo el error más típico en programación
+""")
+# modules#de my_function():
+def sum_two_values(first_number, second_number):
+    print(first_number + second_number)
 
+print("""
+- y se llama facilmente sum: values(5, 3) y se tiene valores dando = 8
+""")
+# modules#de my_function():
+def sum_two_values(first_number, second_number):
+    print(first_number + second_number)
+
+sum_two_values(5, 3)
+
+print("""
+- Esto da resultado 8, la solución no correcta. Si la función cambia se tiene
+    en el futuro se tendría que actualizar en todos los archivos donde se copió.
+- Solución es usar módulos: en lugar de copiar se debe 'importar' desde el archivo original.
+- Al ser copiada en varios archivos se generan duplicación de código
+- Al descubrir un error de lógica se tendría que modificar la función en cada archivo donde se colocó
+- La solución: módulos en lugar de copiar la función se debe guardar en un archivo separado
+- En los archivos que se necesiten se importan usando la palabra reservada import
+
+    import 10_functions   # fallará: porque los modulos empiecen con números
+    
+    sum_two_values(5, 3)
+    
+- Se crea un fichero llamado module.py   
+""")
+
+def sum(numberOne, numberTwo, numberThree):
+    print(numberOne + numberTwo + numberThree)
+
+# A continuación se sustituye el contenido del fichero, por:
+
+def sum(numberOne, numberTwo, numberThree):
+    print(numberOne + numberTwo + numberThree)
+
+print(
+    """
+- Se sustituye el contenido del fichero y al ejecutar ya no se produce error
+#modules#de my_function():
+
+import module
 module.sum(5, 3, 1)
 
+my_module.printValue("Hola Python")
+""")
+
+print("** Al importar una función de un módulo se usa la palabra clave 'from' ")
 from my_module import sum
+sum (1,2,3)
 
-sum(1, 2, 3)
-
-
+print("** O se puede importar por separado **")
 from my_module import sum, printValue
 
 sum(1, 2, 3)
 print("hola python")
 
-
-#Uso del modulo Math
+print("""
+** Uso del modulo Math
+------------------------
+    - En Python existen integrados que ya contienen funciones y constantes listas 
+    para usar, como el módulo math.
+    - La Expresión math.pi representa el valor del número 'pi'. Al utilizar 
+    print(math.pi), mostramos su valor en pantalla.
+    - Al usar math.pow(2, 8) permite calcular una potencia -> 2 elevado a la 8
+""")
 import math
 
 math.pi
@@ -52,8 +111,14 @@ print(math.pow(2, 8))
 from math import pi
 print(pi)
 
-#importacion con alias
+print("""\n
+### importacion con alias ###\n
+    - También se puede importar un elemento de un módulo y asignarle un nombre
+    diferente utilizando la palabra clave 'as' -> llamado 'alias'
+    
+    ** Ejemplo: 
+        se importa la constante pi del módulo math y le damos el nombre  PI_VALUE: 
+# """)
 
 from math import pi as PI_VALUE
 print(PI_VALUE)
-
